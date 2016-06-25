f1f73ff66e3001452eec102b792ba0b1ed528a68
John Metcalf <digital.wilderness@googlemail.com>
2016-06-25 11:45:43 +0100

    Create 16bitsetbit.asm

---
+++ 16bitsetbit.asm
@@ -0,0 +1,11 @@
+; set bit a (0..15) of the bc register (bc = 2^a)
+; self-modifying code
+
+  rlca
+  rlca
+  cp %00100000
+  rla
+  or %11000000
+  ld (SETBIT+1),a
+SETBIT:
+  set 0,b ; this instruction gets modified
